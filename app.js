aaa2374966669d456309bb63db4a41d832823ecd
Quan HL <quan.luuhoang8@gmail.com>
2025-01-22 18:16:18 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const {createServer} = require('http');
+const {createEndpoint} = require('@jambonz/node-client-ws');
+const server = createServer(app);
+const makeService = createEndpoint({server});
+const logger = require('pino')({level: process.env.LOGLEVEL || 'info'});
+const port = process.env.WS_PORT || 3000;
+
+app.locals = {
+  ...app.locals,
+  logger
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./lib/routes')({logger, makeService});
+
+server.listen(port, () => {
+  logger.info(`jambonz websocket server listening at http://localhost:${port}`);
+});
